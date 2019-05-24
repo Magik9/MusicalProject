3440156a113960f393998556b66c21fce10ccb8f
magigabriele_82 <magigabriele90@gmail.com>
2019-05-24 12:09:57 +0200

    no message

--- AngularMusical/MusicProject/src/app/app-routing.module.ts
+++ AngularMusical/MusicProject/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetListComponent } from './get-list/get-list.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { UpdateElementComponent } from './update-element/update-element.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'get-list' },
+  {path: 'get-list', component: GetListComponent},
+  {path: 'post-create', component: PostCreateComponent},
+  {path: 'update-element/:id', component: UpdateElementComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularMusical/MusicProject/src/app/app.module.ts
+++ AngularMusical/MusicProject/src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { GetListComponent } from './get-list/get-list.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { UpdateElementComponent } from './update-element/update-element.component';
+// Components
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetListComponent,
+    PostCreateComponent,
+    UpdateElementComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
