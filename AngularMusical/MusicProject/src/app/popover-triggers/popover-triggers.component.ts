1f759edfa6615f61d453d3c4649d293cf2225f8c
magigabriele_82 <magigabriele90@gmail.com>
2019-06-04 11:21:57 +0200

    popup updated

--- AngularMusical/MusicProject/src/app/popover-triggers/popover-triggers.component.ts
+++ AngularMusical/MusicProject/src/app/popover-triggers/popover-triggers.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RestApiService } from "../shared/rest-api.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-popover-triggers',
   templateUrl: './popover-triggers.component.html',
   styleUrls: ['./popover-triggers.component.css']
 })
-export class PopoverTriggersComponent {
-  @Input() data: any;
+export class PopoverTriggersComponent implements OnInit {
+  @Input() disco: any;
+  Brani: any = [];
+
+  constructor(
+    public restApi: RestApiService,
+    public actRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.loadBraniDisco(this.disco.Id);
+  }
+  loadBraniDisco(discoId)
+  {
+    this.restApi.getBraniDisco(discoId).subscribe((data: {}) => {
+      this.Brani = data;
+  })
+  }
 }
